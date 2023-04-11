9e24dd228d5e9e83d1e43f3ecd578986fde97abc
andrei-mustaca <andrejmustaca6@gmail.com>
2023-04-11 23:08:36 +0300

    Create Program.cs

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример 1
+            //int number = 23;
+            //int a;
+            //a = a + 1;
+            //int x, y, z;
+            //x = y = z= 10;
+            //Пример 2
+            //int value = 10;
+            //value += 10;
+            //Console.WriteLine("value={0}",value);
+            //value -= 5;
+            //Console.WriteLine("value={0}",value);
+            //value *= 10;
+            //Console.WriteLine("value={0}", value);
+            //value /= 3;
+            //Console.WriteLine("value={0}", value);
+            //value %= 8;
+            //Console.WriteLine("value={0}", value);
+            //Пример 3
+            //int i = 3;
+            //Console.WriteLine(i);
+            //Console.WriteLine(i++);
+            //Console.WriteLine(i);
+            //Пример 4
+            //double a = 1.5;
+            //Console.WriteLine(a);
+            //Console.WriteLine(++a);
+            //Console.WriteLine(a);
+            //Пример 5
+            //int i = 3;
+            //Console.WriteLine(i);
+            //Console.WriteLine(i--);
+            //Console.WriteLine(i);
+            //Пример 6
+            //double a = 1.5;
+            //Console.WriteLine(a);
+            //Console.WriteLine(--a);
+            //Console.WriteLine(a);
+        }
+    }
+}
