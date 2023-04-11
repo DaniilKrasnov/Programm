b4ad81bd79b1f59aaaa0963cc49ce26a7b10da15
andrei-mustaca <andrejmustaca6@gmail.com>
2023-04-11 21:56:52 +0300

    Create Program.cs

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("    #");
+            //Console.WriteLine("    #");
+            //Console.WriteLine("    #");
+            //Console.WriteLine("    #");
+            //Console.WriteLine("    #");
+            //Console.WriteLine("    ###############");
+            //Console.WriteLine("    #");
+            //Console.WriteLine("    #");
+            //Console.WriteLine("    #");
+            //Console.WriteLine("    #");
+            //Console.WriteLine("    #");
+            //Console.WriteLine("    #");
+
+            //Задача 2
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("      ########");
+            //Console.WriteLine("      #");
+            //Console.WriteLine("      #");
+            //Console.WriteLine("      #");
+            //Console.WriteLine("      #");
+            //Console.WriteLine("      #######");
+            //Console.WriteLine("             #");
+            //Console.WriteLine("             #");
+            //Console.WriteLine("             #");
+            //Console.WriteLine("             #");
+            //Console.WriteLine("             #");
+            //Console.WriteLine("       ######");
+
+            //Задача 3
+            //а
+            //int x = 47;
+            //int y = 12;
+            //Console.WriteLine("x="+x);
+            //Console.WriteLine("y="+y);
+            //Console.WriteLine($"{x}+{y}={x+y}");
+            //б
+            //int x = 12;
+            //int y = 14;
+            //Console.WriteLine("x="+x);
+            //Console.WriteLine("y="+y);
+            //Console.WriteLine($"{x}+{y}={y}+{x}");
+            //в
+            //int x = 35;
+            //int y = 15;
+            //int z = 6;
+            //Console.WriteLine("x"+x);
+            //Console.WriteLine("y"+y);
+            //Console.WriteLine("z"+z);
+            //Console.WriteLine($"{x}+{y}+{z}={x+y+z}");
+            //г
+            //double x=2.35;
+            //double y = 4.63;
+            //Console.WriteLine("x="+x);
+            //Console.WriteLine("y="+y);
+            //Console.WriteLine($"{x}*{y}={x*y:f1}");
+            //д
+            //double x = 12.6;
+            //double y = 3.45;
+            //Console.WriteLine("x="+x);
+            //Console.WriteLine("y="+y);
+            //Console.WriteLine($"{x:f3}/{y:f3}={x/y:f3}");
+            //е
+            double x = 13.5;
+            double y = 1.468;
+            double z = 20;
+            Console.WriteLine("x="+x);
+            Console.WriteLine("y="+y);
+            Console.WriteLine("z="+z);
+            Console.WriteLine($"({x:f2}+{y:f2})+{z:f2}={x:f2}+({y:f2}+{z:f2})");
+            Console.ReadKey();
+        }
+    }
+}
