877cadcd4c475e6a56dae2c830b2a1bc7046de86
andrei-mustaca <andrejmustaca6@gmail.com>
2023-04-12 01:06:56 +0300

    Create Program.cs

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //int x=Convert.ToInt32(Console.ReadLine());
+            //if(x>=0)
+            //{
+            //    double f = 2 * x + (Math.Sqrt(x));
+            //    Console.WriteLine("f"+f);
+            //}
+            //if(x<0)
+            //{
+            //    double f=Math.Abs(x)+Math.Pow(2,x);
+            //    Console.WriteLine("f"+f);
+            //}
+
+            //Задача 2
+            //double a=Convert.ToDouble(Console.ReadLine());
+            //if(a<=-5)
+            //{
+            //    double f = Math.Sqrt(13 - a);
+            //    Console.WriteLine("f"+f);
+            //}
+            //if(-5<a&&a<5)
+            //{
+            //    double f = Math.Abs(4 * a - 5);
+            //    Console.WriteLine("f"+f);
+            //}
+            //if(a>=5)
+            //{
+            //    double f = 3 * Math.Pow(2, a) - 5 * a;
+            //    Console.WriteLine("f");
+            //}
+
+            //Задача 3
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //switch(n)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Понедельник");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Вторник");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Среда");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Четверг");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Пятница");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Суббота");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Воскресенье");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Ошибка");
+            //        break;
+            //}
+
+            //Задача 4
+            //int month = 4;
+            //string season;
+
+            //switch (month)
+            //{
+            //    case 12:
+            //    case 1:
+            //    case 2:
+            //        season = "зима";
+            //        break;
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //        season = "весна";
+            //        break;
+            //    case 6:
+            //    case 7:
+            //    case 8:
+            //        season = "лето";
+            //        break;
+            //    case 9:
+            //    case 10:
+            //    case 11:
+            //        season = "осень";
+            //        break;
+            //    default:
+            //        season = "нет месяца";
+            //        break;
+            //}
+
+            //Console.WriteLine("Время года-"+season);
+            Console.ReadKey();
+        }
+    }
+}
