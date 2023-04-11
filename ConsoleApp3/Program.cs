132b1b5d28f7c5f773173f54251b048e880daac9
andrei-mustaca <andrejmustaca6@gmail.com>
2023-04-12 00:10:49 +0300

    Create Program.cs

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if(a>0)
+            //{
+            //    Console.WriteLine("Число положительное");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Число отрицательное или нулевое");
+            //}
+
+            //Пример 2
+            //int a=Convert.ToInt32(Console.ReadLine());
+            //int b=Convert.ToInt32(Console.ReadLine());
+            //if(a==b)
+            //{
+            //    Console.WriteLine("Числа равны");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("Числа неравны");
+            //}
+
+            //Задача 3
+            //int a=Convert.ToInt32(Console.ReadLine());
+            //int b=Convert.ToInt32(Console.ReadLine());
+            //int c=Convert.ToInt32(Console.ReadLine());
+            //if(a==b&&b==c&&a==c)
+            //{
+            //    Console.WriteLine("YES");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("NO");
+            //}
+
+            //Задача 4
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if(a>0)
+            //{
+            //    a= a+ 1;
+            //    Console.WriteLine("a="+a);
+            //}
+            //else
+            //{
+            //    a = a - 2;
+            //    Console.WriteLine("a="+a);
+            //}
+
+            //Пример 5
+            //int a=Convert.ToInt32(Console.ReadLine());
+            //if(a>0)
+            //{
+            //    a = a + 1;
+            //    Console.WriteLine("a"+a);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("a"+a);
+            //}
+
+            //Задача 6
+            //int a=Convert.ToInt32(Console.ReadLine());
+            //if(a==0)
+            //{
+            //    Console.WriteLine("Нулевое число");
+            //} 
+            //if(a<0)
+            //{
+            //    Console.WriteLine("Отрицательное число");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Положительное число");
+            //}
+
+            //Задача 8
+            //int a=Convert.ToInt32(Console.ReadLine());
+            //int b=Convert.ToInt32(Console.ReadLine());
+            //if(a>b)
+            //{
+            //    Console.WriteLine("Число a больше числа b");
+            //}
+            //if(a<b)
+            //{
+            //    Console.WriteLine("Число b больше числа a");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Ошибка");
+            //}
+            Console.ReadKey();
+        }
+    }
+}
