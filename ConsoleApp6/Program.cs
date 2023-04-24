dec619a8999a3b13017d7684ec1c142342caa381
andrei-mustaca <andrejmustaca6@gmail.com>
2023-04-24 07:04:27 +0300

    Create Program.cs

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //int n = Convert.ToInt32(Console.Read());
+            //for(int i=1; i<n; i++)
+            //{
+            //    Console.WriteLine("Привет");
+            //}
+            //3
+            //int a =11;
+            //for(int i=1;i<9;i++)
+            //{
+            //    a = a + 11;
+            //    Console.WriteLine(a);
+            //}
+            //4
+            //int a = 10;
+            //int b = 0;
+            //for(int i = 0; i < 90; i++)
+            //{
+            //    a = a + 1;
+            //    if(a%16==0)
+            //    {
+            //        b++;
+            //        Console.WriteLine(a);
+            //    }
+            //}
+            //Console.WriteLine(b);
+            //5
+            //int a = 10;
+            //int b = 0;
+            //int summ = 0;
+            //for(int i=1;i<90;i++)
+            //{
+            //    a = a + 1;
+            //    if(a%25==0)
+            //    {
+            //        b++;
+            //        summ = a + a;
+            //        Console.WriteLine(a);
+            //    }
+            //}
+            //Console.WriteLine(b);
+            //Console.WriteLine(summ);
+
+            Console.ReadKey();
+        }
+    }
+}
