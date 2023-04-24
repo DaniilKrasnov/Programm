91f78c5ac684190343ab2dcae784ed957d7c124a
andrei-mustaca <andrejmustaca6@gmail.com>
2023-04-24 06:12:49 +0300

    Create Program.cs

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //byte b = 255;
+            //2
+            //int intVar = 5;
+            //double doubleVar = 0.15;
+            //3
+            //string s="Hi,everybody";
+            //char c = 'R';
+            //Console.WriteLine(s);
+            //Console.WriteLine(c);
+            //4
+            //const double PI = 3.14;
+            //5
+            //double op1 = 4.6;
+            //double op2 = -34.72;
+            //double sum, dif, mul, div;
+            //sum = op1 + op2;
+            //dif = op1 - op2;
+            //mul = op1 * op2;
+            //div = op1 / op2;
+            //Console.WriteLine($"{op1}+{op2}={sum}");
+            //Console.WriteLine($"{op1}-{op2}={dif}");
+            //Console.WriteLine($"{op1}*{op2}={mul}");
+            //Console.WriteLine($"{op1}/{op2}={div}");
+            Console.ReadKey();
+        }
+    }
+}
