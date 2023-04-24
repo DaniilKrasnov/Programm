112cbb92da9118e8d1eb72ab7726fad75eefdfc0
andrei-mustaca <andrejmustaca6@gmail.com>
2023-04-24 07:26:31 +0300

    Create Program.cs

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //for(int i=0;i<18;i++)
+            //{
+            //    Console.Write("#");
+            //}
+            //2
+            //for(int i=0;i<25;i++)
+            //{
+            //    Console.WriteLine("#");
+            //}
+            //3
+            //for(int i=0;i<4;i++)
+            //{
+            //    Console.WriteLine();
+            //}
+            //for(int i=0;i<18;i++)
+            //{
+            //    Console.Write("#");
+            //}
+            //4
+            //for(int i=0;i<18;i++)
+            //{
+            //    Console.WriteLine("     #");
+            //}
+            //5
+            //for(int i=0;i<5;i++)
+            //{
+            //    Console.WriteLine("");
+            //    for(int a=0;a<18;a++)
+            //    {
+            //        Console.Write("#");
+            //    }
+            //}
+            //6
+            Console.ReadKey();
+        }
+    }
+}
