50aab45c204336b04ea4ed82a2621d7137105058
andrei-mustaca <andrejmustaca6@gmail.com>
2023-04-26 21:50:31 +0300

    Create Program.cs

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //int[] mass = new int[6];
+            //for(int i=0;i<6;i++)
+            //{
+            //    Console.WriteLine($"mass[{i}]=");
+            //    mass[i]=Convert.ToInt32(Console.ReadLine());
+            //}
+            //for(int i=0;i<6;i++)
+            //{
+            //    Console.WriteLine($"mass[{i}]={mass[i]}");
+            //}
+
+            //2
+            //int[] mass = new int[12];
+            //Random random = new Random();
+            //for(int i=0;i<12;i++)
+            //{
+            //    mass[i]=random.Next(163,190);
+            //    Console.WriteLine($"mass[{i}]={mass[i]}см");
+            //}
+
+            //3
+            //double[] mass = new double[10];
+            //Random random = new Random();
+            //for(int i=0; i < 10; i++)
+            //{
+            //    mass[i] = random.Next(-50,50);
+            //}
+            //Console.WriteLine($"mass[{0}]={mass[0]}");
+            //Console.WriteLine($"mass[{9}]={mass[9]}");
+
+            //4
+            //int[] mass = new int[] {37,0,50,46,34,46,0,13};
+            //foreach (int elem in mass)
+            //{
+            //    Console.WriteLine($"mas]={elem}");
+            //}
+            Console.ReadKey();
+        }
+    }
+}
